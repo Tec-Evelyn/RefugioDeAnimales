ae2af3661f0314f7899c673169194d96e2322915
Mónica <monicaramos@outlook.com>
2023-08-21 11:30:15 -0600

    Codificación de los elementos del paquete Utils del Proyecto AppWeb

--- RefugioDeAnimales.AppWeb/src/java/refugiodeanimales/appweb/controllers/MascotaServlet.java
+++ RefugioDeAnimales.AppWeb/src/java/refugiodeanimales/appweb/controllers/MascotaServlet.java
@@ -8,6 +8,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
+import refugiodeanimales.accesoadatos.TipoDAL;
+import refugiodeanimales.accesoadatos.GeneroDAL;
+import refugiodeanimales.accesoadatos.MascotaDAL;
+import refugiodeanimales.appweb.utils.*;
+import refugiodeanimales.entidadesdenegocio.Tipo;
+import refugiodeanimales.entidadesdenegocio.Genero;
+import refugiodeanimales.entidadesdenegocio.Mascota;
+
 @WebServlet(name = "MascotaServlet", urlPatterns = {"/Mascota"})
 public class MascotaServlet extends HttpServlet {
 
